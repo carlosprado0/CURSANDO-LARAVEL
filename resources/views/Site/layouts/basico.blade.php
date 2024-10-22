c58fb399f629985556c5500d638d74c3579855ed
Marcelo silva <msr88957@gmail.com>
2024-10-22 10:55:15 -0300

    Layouts, yield, sections e extends GG

--- resources/views/Site/layouts/basico.blade.php
+++ resources/views/Site/layouts/basico.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>MarketPlace - Sobre Nós</title>
+        <title>MarketPlace - @yield('titulo')</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
     </head>
