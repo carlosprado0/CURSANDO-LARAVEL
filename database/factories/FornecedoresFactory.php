cecee9d4a59ff3ac3036f0f41d36e522e8bf8c78
Marcelo silva <msr88957@gmail.com>
2024-10-30 12:00:15 -0300

    eloquent

---
+++ database/factories/FornecedoresFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factory;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory*/
+
+use App\Models\Fornecedores;
+use Faker\Generator as Faker;
+
+        $factory->define(Fornecedores::class, function (Faker $faker){
+        return [
+            'name' => 
+            'site' => 
+            'uf' => 
+            'email' => 
+    ];
+    });
---
+++ database/seeders/FornecedoresSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Fornecedores;
+
+class FornecedoresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    $fornecedores= new fornecedores();
+    $fornecedores->name = 'Carlos';
+    $fornecedores->site = 'carlosfornecedorb2b.com.br';
+    $fornecedores->uf = 'GO';
+    $fornecedores->email = 'carlosfornecedor@contato.com';
+    $fornecedores->save();
+    }
+}
